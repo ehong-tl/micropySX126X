13ea5601fcf88c05e2ebbf7b1ba0df9e35498ee8
ehong-tl <ehong@ntu.edu.sg>
2020-03-19 14:53:04 +0800

    Fixed startReceive: setPacketParamsFSK

--- lib/sx126x.py
+++ lib/sx126x.py
@@ -375,7 +375,7 @@ class SX126X:
                 
             state = self.setPacketParams(self._preambleLength, self._crcType, self._implicitLen, self._headerType, self._invertIQ)
         elif modem == SX126X_PACKET_TYPE_GFSK:
-            state = self.setPacketParamsFSK(self._preambleLengthFSK, self._crcTypeFSK, self._syncWordLength, self._addrComp, self._whitening, self._packetType, len_)
+            pass
         else:
             return ERR_UNKNOWN
         ASSERT(state)
